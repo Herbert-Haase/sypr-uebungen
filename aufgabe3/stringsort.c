0d617ffe3d4bcb6556af4f88d9e8d11de498caf8
Herbert-Haase <herberthaase02@gmx.de>
2025-04-25 23:18:39 +0200

    stringsort.c gemäß den Anweisungen nachgebessert

--- aufgabe3/stringsort.c
+++ aufgabe3/stringsort.c
@@ -4,9 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-#define NUM_LEN 20
-
-void bubblesort(int n, int m, char *a);
+void bubblesort(int n, int m, char **a);
 
 int main(int argc, const char *argv[]) {
   if (argc != 2) {
@@ -16,11 +14,12 @@ int main(int argc, const char *argv[]) {
     return EXIT_FAILURE;
   }
   int n = atoi(argv[1]);
+  int m = strlen(argv[1]) + 1;
   if (n < 1) {
     printf("Anzahl muss mindestens 1 sein\n");
     return EXIT_FAILURE;
   }
-  char(*a)[NUM_LEN] = malloc(n * sizeof(char[NUM_LEN]));
+  char **a = malloc(n * sizeof(char*));
   if (!a) {
     fprintf(stderr, "out of memory");
     exit(1);
@@ -31,12 +30,17 @@ int main(int argc, const char *argv[]) {
 
   int str_amount = 0;
   for (int i = 0; i < n; ++i) {
-    int r = rand() % 10;
-    str_amount += sprintf(*(a + i), "%d", r);
-    printf("%s ", *(a + i));
+    a[i] = malloc(m * sizeof(char));
+    if (!a[i]) {
+      fprintf(stderr, "out of memory");
+      exit(1);
+    }
+    int r = rand() % n;
+    str_amount += sprintf(a[i], "%d", r);
+    printf("%s ", a[i]);
   }
   puts("\nSorted");
-  bubblesort(n, NUM_LEN, &a[0][0]);
+  bubblesort(n, m, (char **)a);
 
   char *strbuilder = malloc(2 * str_amount * sizeof(char) + 1);
   if (!strbuilder) {
@@ -45,11 +49,11 @@ int main(int argc, const char *argv[]) {
   }
   strcpy(strbuilder, *a);
   for (int i = 1; i < n; ++i) {
-    if (!strcmp(*(a + i - 1), *(a + i))) {
+    if (!strcmp(a[i - 1], a[i])) {
       strcat(strbuilder, "*");
     } else {
       strcat(strbuilder, " ");
-      strcat(strbuilder, *(a + i));
+      strcat(strbuilder, a[i]);
     }
   }
   printf("%s\n", strbuilder);
@@ -57,12 +61,12 @@ int main(int argc, const char *argv[]) {
   free(a);
 }
 
-void bubblesort(int n, int m, char *a) {
-  char tmp[NUM_LEN];
+void bubblesort(int n, int m, char **a) {
+  char *tmp = malloc(m * sizeof(char));
   for (int i = n; i > 1; --i) {
-    for (int j = 0; j < (i - 1); ++j) {
-      char *str1 = a + (j * m);
-      char *str2 = a + ((j + 1) * m);
+    for (int j = 0; j < i - 1; ++j) {
+      char *str1 = a[j];
+      char *str2 = a[j + 1];
       if (strcmp(str1, str2) > 0) {
         strcpy(tmp, str2);
         strcpy(str2, str1);
@@ -70,4 +74,5 @@ void bubblesort(int n, int m, char *a) {
       }
     }
   }
+  free(tmp);
 }
